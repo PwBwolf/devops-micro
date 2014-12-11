2cf4f7a78916ddf5ca4abfc2adaa76387d11467c
Achinth Gurkhi <achinth@gmail.com>
2014-12-11 17:47:21 +0530

    server refactoring

--- daemons/rule-engine/env/integration.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    environment: 'integration',
-    port: 8001,
-    db: 'mongodb://localhost/yiptv',
-    url: 'http://int.yiptv.net/',
-    imageUrl: 'http://int.yiptv.net/img/'
-};
--- daemons/rule-engine/env/production.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    environment: 'production',
-    port: 8001,
-    db: 'mongodb://localhost/yiptv',
-    url: 'http://yiptv.com/',
-    imageUrl: 'http://yiptv.com/img/'
-};
