fa0f6f266896c065adb3548d4f5b9b518b0778b4
Vivek Katta <vivek@yiptv.com>
2014-12-18 12:15:21 +0530

    decoupling fixture loading

--- server/common/database/fixtures.js
+++ server/common/database/fixtures.js
@@ -7,40 +7,38 @@ var models = {
     "AppConfig": mongoose.model('AppConfig')
 }
 
-module.exports = function() {
-    var options = {
-        followLinks: false,
-        listeners: {
-            file: function (root, fileStats, next) {
-                if(fileStats.name.indexOf('.json') > -1) {
-                    var collectionContent = fs.readJSONSync(__dirname + '/' + fileStats.name);
-                    if(models[collectionContent.type]) {
-                        models[collectionContent.type].collection.remove(function(err, result){
-                            if(err) {
-                                console.log('Could not delete '+collectionContent.type+' first...file not processed');
+var options = {
+    followLinks: false,
+    listeners: {
+        file: function (root, fileStats, next) {
+            if(fileStats.name.indexOf('.json') > -1) {
+                var collectionContent = fs.readJSONSync(__dirname + '/' + fileStats.name);
+                if(models[collectionContent.type]) {
+                    models[collectionContent.type].collection.remove(function(err, result){
+                        if(err) {
+                            console.log('Could not delete '+collectionContent.type+' first...file not processed');
+                            next();
+                        } else {
+                            models[collectionContent.type].collection.insert(collectionContent.docs, {}, function(err, docs) {
+                                if(err) {
+                                    console.log('Something went wrong when inserting ' + fileStats.name + ' to the database!');
+                                }
+                                console.log('Successfully inserted ' + fileStats.name + ' to the database!');
                                 next();
-                            } else {
-                                models[collectionContent.type].collection.insert(collectionContent.docs, {}, function(err, docs) {
-                                    if(err) {
-                                        console.log('Something went wrong when inserting ' + fileStats.name + ' to the database!');
-                                    }
-                                    console.log('Successfully inserted ' + fileStats.name + ' to the database!');
-                                    next();
-                                });
-                            }
-                        });
-
-                    }
+                            });
+                        }
+                    });
 
                 }
 
-            },
-            errors: function (root, nodeStatsArray, next) {
-                next();
             }
-        }
-    };
 
-    // create a route using the crawler
-    var route  = crawler.walkSync('../common/database', options);
-}
+        },
+        errors: function (root, nodeStatsArray, next) {
+            next();
+        }
+    }
+};
+
+// create a route using the crawler
+var route  = crawler.walkSync('../common/database', options);
