c6480c01fea966bbf194a13a5464e5ba5677ee6b
varun <varunv@yiptv.com>
2014-12-20 17:54:55 +0530

    completed unit tests for contact us page

--- test/client/unit/controllers/contact-us.js
+++ test/client/unit/controllers/contact-us.js
@@ -10,15 +10,22 @@ describe('Controller: contactUsCtrl', function () {
         q,
         appService,
         loggerService,
-        httpBackend;
+        httpBackend,
+        location,
+        loggerSvc;
 
     // Initialize the controller and a mock scope
-    beforeEach(inject(function ($controller, $rootScope, appSvc, $httpBackend, $http) {
+    beforeEach(inject(function ($controller, $rootScope, appSvc, $httpBackend, $location) {
         scope = $rootScope.$new();
         controller = $controller;
-        loggerService = {};
+        loggerService = {
+            logError: function (message) {
+
+            }
+        };
         httpBackend = $httpBackend;
         appService = appSvc;
+        location = $location;
     }));
 
     function setupController() {
@@ -36,8 +43,40 @@ describe('Controller: contactUsCtrl', function () {
         expect(scope.mv.country).toBe('United States');
     });
 
+    it('should send the form', function () {
+
+        it('should set the form dirty', function () {
+            setupController();
+            scope.form.$valid = false;
+            scope.saveContactUs();
+            expect(scope.form.name.$dirty).toBe(true);
+            expect(scope.form.interest.$dirty).toBe(true);
+            expect(scope.form.email.$dirty).toBe(true);
+            expect(scope.form.telephone.$dirty).toBe(true);
+            expect(scope.form.details.$dirty).toBe(true);
+        });
+
+        it('should send the form data to server', function () {
+            httpBackend.when("POST",'/api/save-contact-us').respond(200,[]);
+            setupController();
+            scope.form.$valid = true;
+            scope.saveContactUs();
+            expect(scope.saving).toBe(false);
+            expect(location.path).toBe('/contact-us-success')
+        });
+
+    });
+
+    /*it('should show appropriate error message on error', function () {
+        httpBackend.when("GET","/api/get-countries").respond(404,[]);
+        setupController();
+        httpBackend.flush();
+    });
+
     it('should show appropriate error message on error', function () {
         httpBackend.when("GET","/api/get-countries").respond(404,[]);
-    });
+        setupController();
+        httpBackend.flush();
+    });*/
 
 });
