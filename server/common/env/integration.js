f3dea2f89d3e7213c1df88ce57b6329446df5131
Achinth Gurkhi <achinth@gmail.com>
2014-12-22 16:14:04 +0530

    update env specific config file

--- server/common/env/integration.js
+++ server/common/env/integration.js
@@ -15,6 +15,11 @@ module.exports = {
     contactUsEmailList: 'achinth@yiptv.com, vivek@yiptv.com',
     cloudSpongeDomainKey: 'BNDQ7QY3HH38QG45E3P8',
     aioUrl: 'http://yiptv.net',
-    aioApiKey: 'xxxx',
+    aioUserPin: '1234',
+    aioApiKey: '10bf5e4e05b1fe32f9c88e1355fd30e40549041e5',
+    aioFreePackages: [{'packageid' : 27}, {'packageid' : 62}],
+    aioUnlimitedPackages: [{'packageid' : 1}, {'packageid' : 27}, {'packageid' : 62}],
+    yipFreePackageId: 1,
+    yipUnlimitedPackageId: 2,
     cleanup: ['Account', 'User', 'ContactUs', 'Visitor']
 };
