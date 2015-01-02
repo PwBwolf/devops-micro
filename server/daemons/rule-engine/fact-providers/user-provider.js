6b1d2562e8a91998c422677f02eb4d89790eec0a
Vivek Katta <vivek@yiptv.com>
2015-01-02 13:44:02 +0530

    fixed a bug with emailer

--- server/daemons/rule-engine/fact-providers/user-provider.js
+++ server/daemons/rule-engine/fact-providers/user-provider.js
@@ -11,8 +11,7 @@ function populateAccount(account) {
     var def = Q.defer();
     account.populate('primaryUser', function(err, populatedAccount){
         if(!err) {
-            populatedAccount.primaryUser = _.assign({doctype: 'user'}, populatedAccount.primaryUser._doc);
-            console.log(populatedAccount);
+            populatedAccount.primaryUser._doc = _.assign(populatedAccount.primaryUser._doc, {doctype: 'user'});
             def.resolve(populatedAccount);
         } else {
             def.reject(err);
@@ -24,14 +23,14 @@ function populateAccount(account) {
 module.exports.getFreeUsers = function() {
     var def = Q.defer();
     Account.find({type: "free"}).exec().then(function(accounts) {
-        if(users) {
+        if(accounts) {
             var accountPromises = [];
             var userList = [];
             for(var i = 0; i < accounts.length; i++) {
                 var accountPromise = populateAccount(accounts[i]);
                 accountPromises.push(accountPromise);
                 accountPromise.then(function(account) {
-                   userList.push(account.primaryUser);
+                   userList.push(account.primaryUser._doc);
                 }, function (err) {
                     console.log('Something went wrong when retrieving user info for account:', err);
                 });
