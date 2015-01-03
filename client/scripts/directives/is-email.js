1646a50046c8cf6dc5b09f28fecf8bbdb7726f63
Achinth Gurkhi <achinth@gmail.com>
2015-01-03 11:24:46 +0530

    defect fixes

--- client/scripts/directives/is-email.js
+++ client/scripts/directives/is-email.js
@@ -8,7 +8,8 @@
             link: function (scope, elm, attrs, ctrl) {
 
                 ctrl.$parsers.unshift(function (email) {
-                    var isEmail = /^\w+([\+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
+                    var isEmail = regex.test(email);
                     ctrl.$setValidity('isEmail', isEmail);
                     return email;
                 });
