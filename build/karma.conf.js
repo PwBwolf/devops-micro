2d5646ad9360c34c039c414d292b4a47f2f903b7
varun <varunv@yiptv.com>
2015-01-04 18:53:26 +0530

    added all unit tests for main controller

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
         '../client/bower_components/jquery/jquery.js',
+        '../client/bower_components/lodash/dist/lodash.js',
         '../client/bower_components/angular/angular.js',
         '../client/bower_components/angular-webstorage/angular-webstorage.js',
         '../client/bower_components/angular-sanitize/angular-sanitize.js',
