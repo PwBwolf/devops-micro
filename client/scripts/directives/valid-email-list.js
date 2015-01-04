9e0829eaf4b12eb7bc70a22bf5e9171cbd9c00e7
Achinth Gurkhi <achinth@gmail.com>
2015-01-04 20:18:16 +0530

    fix issue #326

--- client/scripts/directives/valid-email-list.js
+++ client/scripts/directives/valid-email-list.js
@@ -8,10 +8,13 @@
             link: function (scope, elm, attrs, ctrl) {
 
                 ctrl.$parsers.unshift(function (emailList) {
-                    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
                     var emails = emailList.split(',');
+                    console.dir(emails);
                     for (var i = 0; i < emails.length; i++) {
+                        var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
+                        console.log(emails[i].trim());
                         var isEmail = regex.test(emails[i].trim());
+                        console.log(isEmail);
                         if (!isEmail) {
                             ctrl.$setValidity('validEmailList', false);
                             return emailList;
