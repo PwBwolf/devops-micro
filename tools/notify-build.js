0044c82ccba7e544b710a9853a094681ee4c3ecf
Vivek Katta <vivek@yiptv.com>
2015-01-07 19:18:03 +0530

    fixed problems with new build notify email code

--- tools/notify-build.js
+++ tools/notify-build.js
@@ -1,19 +1,19 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var config = require('../server/common/config/config');
 var email = require('../server/common/services/email');
 
-var moment = require('moment');
-var sf = require('sf');
-var fs = require('fs-extended');
-
-var env = process.env.NODE_ENV || 'development';
+var moment = require('../server/node_modules/moment');
+var sf = require('../server/node_modules/sf');
+var fs = require('../server/node_modules/fs-extended');
 
 var versionJSON = fs.readJSONSync('../client/version.json');
 
 var mailOptions = {
     from: config.email.fromName + ' <' + config.email.fromEmail + '>',
     to: 'devteam@yiptv.com',
-    subject: sf('New build ({0}) deployed to {1}', versionJSON.version, env),
-    html: sf('New build ({0}) deployed to {1} on {2}', versionJSON.version, argv.env, moment().format('MMMM Do YYYY, h:mm:ss a'));
+    subject: sf('New build ({0}) deployed to {1}', versionJSON.version, process.env.NODE_ENV),
+    html: sf('New build ({0}) deployed to {1} on {2}', versionJSON.version, process.env.NODE_ENV, moment().format('MMMM Do YYYY, h:mm:ss a'))
 };
 
 email.sendEmail(mailOptions, function (err) {
