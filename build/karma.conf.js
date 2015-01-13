b1972535ff98cf3d9e33a250c118a6e54fd1efdf
Vivek Katta <vivek@yiptv.com>
2015-01-13 14:23:32 +0530

    unit testing does not need chrome

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -70,10 +70,9 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: [],
 
     plugins: [
-          'karma-chrome-launcher',
           'karma-jasmine'
     ],
 
