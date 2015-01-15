89b9e42540a7f0fc177f187384983c4ae47b2f28
Achinth Gurkhi <achinth@gmail.com>
2015-01-15 06:30:27 +0530

    product config changes

--- server/common/env/production.js
+++ server/common/env/production.js
@@ -15,11 +15,11 @@ module.exports = {
     secretToken: 'yip#tv@prod#848',
     contactUsEmailList: 'tribs@yiptv.com, carmen@yiptv.com',
     cloudSpongeDomainKey: 'GMQU3G2VW72CLFRVWS2N',
-    aioUrl: 'http://yiptv.com',
+    aioUrl: 'http://content.yiptv.com',
     freeSideApiKey: 'yip-freeside-prod',
-    freeSideUrl: '10.100.10.15:8008',
+    freeSideUrl: 'http://10.100.10.15:8008/',
     aioUserPin: '1234',
     aioApiKey: '10bf5e4e05b1fe32f9c88e1355fd30e40549041e5',
-    aioFreePackages: [{'packageid' : 27}, {'packageid' : 62}],
-    aioPaidPackages: [{'packageid' : 1}, {'packageid' : 27}, {'packageid' : 62}]
+    aioFreePackages: [{'packageid' : 1}, {'packageid' : 66}, {'packageid' : 27}],
+    aioPaidPackages: [{'packageid' : 1}, {'packageid' : 66}, {'packageid' : 62}, {'packageid' : 27}]
 };
