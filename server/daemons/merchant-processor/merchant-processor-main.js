788ea9aeca3005118b98f1c4f978e2440436dbb3
Achinth Gurkhi <achinth@gmail.com>
2015-04-29 13:42:33 +0530

    defect fixes

--- server/daemons/merchant-processor/merchant-processor-main.js
+++ server/daemons/merchant-processor/merchant-processor-main.js
@@ -4,6 +4,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var monq = require('monq'),
     moment = require('moment'),
+    math = require('mathjs'),
     mongoose = require('mongoose'),
     logger = require('../../common/config/logger'),
     config = require('../../common/config/config'),
@@ -27,7 +28,7 @@ worker.register({
                 if (err) {
                     logger.logError('merchantProcessorMain - addUser - validation error');
                     logger.logError(err);
-                    saveToDatabase(params, 'failure', err, function () {
+                    saveToDatabase(params, 'failure', err, true, function () {
                         callback(new Error(err));
                     });
                 } else {
@@ -36,7 +37,7 @@ worker.register({
                         if (err) {
                             callback(err);
                         } else if (result) {
-                            saveToDatabase(params, 'failure', 'username-exists', function () {
+                            saveToDatabase(params, 'failure', 'username-exists', true, function () {
                                 callback(new Error('username-exists'));
                             });
                         } else {
@@ -44,7 +45,7 @@ worker.register({
                                 if (err) {
                                     callback(err);
                                 } else {
-                                    saveToDatabase(params, 'success', '', function () {
+                                    saveToDatabase(params, 'success', '', true, function () {
                                         callback(null, 'success');
                                     });
                                 }
@@ -84,17 +85,17 @@ function addUserInputValidation(params, cb) {
     var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
     var nameRegex = /^[a-zA-Z0-9\s\-,.']+$/;
     var phoneRegex = /^[2-9]{1}[0-9]{2}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
-    if (!params.username || !emailRegex.test(params.username)) {
+    if (!params.username || !emailRegex.test(params.username) || params.username.trim().length > 50) {
         cb('invalid-username');
-    } else if (!params.firstName || !nameRegex.test(params.firstName)) {
+    } else if (!params.firstName || !nameRegex.test(params.firstName) || params.firstName.trim().length > 20) {
         cb('invalid-first-name');
-    } else if (!params.lastName || !nameRegex.test(params.lastName)) {
+    } else if (!params.lastName || !nameRegex.test(params.lastName) || params.lastName.trim().length > 20) {
         cb('invalid-last-name');
-    } else if (!params.telephone || !phoneRegex.test(params.telephone)) {
+    } else if (!params.telephone || !phoneRegex.test(params.telephone) || params.telephone.trim().length > 12) {
         cb('invalid-telephone');
     } else if (!params.amount || typeof params.amount !== 'number') {
         cb('invalid-amount');
-    } else if (params.amount % 14.99 !== 0) {
+    } else if (math.mod(params.amount, 14.99) !== 0) {
         cb('invalid-amount');
     } else if (!params.submitTime || !moment(params.submitTime).isValid()) {
         cb('invalid-submit-time');
@@ -107,11 +108,11 @@ function addUserInputValidation(params, cb) {
 
 function makePaymentInputValidation(params, cb) {
     var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm;
-    if (!params.username || !emailRegex.test(params.username)) {
+    if (!params.username || !emailRegex.test(params.username) || params.username.trim().length > 50) {
         cb('invalid-username');
     } else if (!params.amount || typeof params.amount !== 'number') {
         cb('invalid-amount');
-    } else if (params.amount % 14.99 !== 0) {
+    } else if (math.mod(params.amount, 14.99) !== 0) {
         cb('invalid-amount');
     } else if (!params.submitTime || !moment(params.submitTime).isValid()) {
         cb('invalid-submit-time');
@@ -130,7 +131,7 @@ function doesUserExist(email, cb) {
     });
 }
 
-function saveToDatabase(params, isSuccess, reason, cb) {
+function saveToDatabase(params, isSuccess, reason, isAddUser, cb) {
     var ownedBy;
     NewUser.find({username: params.username}, function (err, dbUser) {
         if (err) {
@@ -143,22 +144,36 @@ function saveToDatabase(params, isSuccess, reason, cb) {
             ownedBy = null;
         }
         var processTime = (new Date()).toUTCString();
-        var payment = new MakePayment(params);
+        var payment = new MakePayment();
+        payment.merchant = params.merchantId;
+        payment.merchantPopId = params.merchantPopId;
+        payment.merchantReferenceId = params.merchantReferenceId;
+        payment.username = params.username;
         payment.status = isSuccess;
         payment.reason = reason;
         payment.processTime = processTime;
         payment.isUserOwned = payment.merchant === ownedBy;
+        payment.amount = (!params.amount || typeof params.amount !== 'number') ? 0 : params.amount;
+        payment.submitTime = (!params.submitTime || !moment(params.submitTime).isValid()) ? '0' : params.submitTime;
+        payment.currency = params.currency;
+        payment.payload = JSON.stringify(params);
         payment.save(function (err) {
             if (err) {
                 logger.logError('merchantProcessorMain - saveToDatabase - error saving MakePayment');
                 logger.logError(err);
                 cb(err);
             } else {
-                if (params.firstName) {
-                    var user = new NewUser(params);
+                if (isAddUser) {
+                    var user = new NewUser();
+                    user.merchant = params.merchantId;
+                    user.merchantPopId = params.merchantPopId;
+                    user.merchantReferenceId = params.merchantReferenceId;
+                    user.username = params.username;
                     user.status = isSuccess;
                     user.reason = reason;
                     user.processTime = processTime;
+                    user.submitTime = (!params.submitTime || !moment(params.submitTime).isValid()) ? '0' : params.submitTime;
+                    user.payload = JSON.stringify(params);
                     user.save(function (err1) {
                         if (err1) {
                             logger.logError('merchantProcessorMain - saveToDatabase - error saving NewUser');
