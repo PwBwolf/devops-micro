73054932e10e6d3290b8673ddc533d22a27dd987
ntlygensYiptv <alden@yiptv.com>
2015-05-12 10:06:40 -0400

    site ypdate

--- server/common/setup/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var _ = require('lodash');
-
-module.exports = _.extend(
-    require(__dirname + '/../config/all.js'),
-    require(__dirname + '/../config/' + process.env.NODE_ENV + '.js') || {}
-);
