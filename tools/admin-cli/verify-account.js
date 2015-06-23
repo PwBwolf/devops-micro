7c8cc0110362431f690f0bd02faa8de1fded5ac8
Achinth Gurkhi <achinth@gmail.com>
2015-06-23 17:00:01 +0530

    send email notifications for verification, payment failure, upgrade, reactivate, convert to complimentary

--- tools/admin-cli/verify-account.js
+++ tools/admin-cli/verify-account.js
@@ -2,9 +2,11 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var config = require('../../server/common/setup/config'),
+var sf = require('sf'),
+    config = require('../../server/common/setup/config'),
     logger = require('../../server/common/setup/logger'),
     aio = require('../../server/common/services/aio'),
+    emailService = require('../../server/common/services/email'),
     mongoose = require('../../server/node_modules/mongoose');
 
 logger.cli();
@@ -69,10 +71,38 @@ Users.findOne({email: email.toLowerCase()}, function (err, user) {
                         });
                     } else {
                         logger.logInfo('adminCLI - verifyAccount - account verified successfully');
-                        process.exit(0);
+                        sendAccountVerifiedEmail(user, function (err) {
+                            if (err) {
+                                logger.logError('adminCLI - verifyAccount - error sending email');
+                            } else {
+                                logger.logInfo('adminCLI - verifyAccount - email sent to user');
+                                process.exit(0);
+                            }
+                        });
                     }
                 });
             }
         });
     }
 });
+
+function sendAccountVerifiedEmail(user, cb) {
+    var signInUrl = config.url + 'sign-in?email=' + encodeURIComponent(user.email);
+    var mailOptions = {
+        from: config.email.fromName + ' <' + config.email.fromEmail + '>',
+        to: user.email,
+        subject: config.accountVerifiedEmailSubject[user.preferences.defaultLanguage],
+        html: sf(config.accountVerifiedEmailBody[user.preferences.defaultLanguage], config.imageUrl, user.firstName, user.lastName, signInUrl)
+    };
+    emailService.sendEmail(mailOptions, function (err) {
+        if (err) {
+            logger.logError('userController - sendAccountVerifiedEmail - error sending email: ' + user.email);
+            logger.logError(err);
+        } else {
+            logger.logInfo('userController - sendAccountVerifiedEmail - email sent successfully: ' + user.email);
+        }
+        if (cb) {
+            cb(err);
+        }
+    });
+}
