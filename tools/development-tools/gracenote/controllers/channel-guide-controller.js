220bcb30d0cb79c31ab356f3d24947be51ccb464
wpiyong <yliu@yiptv.com>
2015-08-12 11:50:32 -0400

    update

--- tools/development-tools/gracenote/controllers/channel-guide-controller.js
+++ tools/development-tools/gracenote/controllers/channel-guide-controller.js
@@ -110,10 +110,6 @@ module.exports = {
         var endTime = req.query.period === undefined ? date.isoDate(dateAdd(nowTime, 'day', 15)) : date.isoDate(dateAdd(nowTime, req.query.hour ? 'hour' : 'day', req.query.period));
         logger.logInfo('getChannelInfo endTime:' + endTime);
 
-        //Channel.find({stationId : req.query.stationId, airings: {$elemMatch: {endTime: {$gte: startTime, $lte: endTime}}}}, {_id : false, 'airings.program.preferredImage.uri' : true, 'airings.endTime' : true, 'airings.startTime' : true, 'airings.program.tmsId' : true, 'airings.program.title' : true, callSign : true }, function(err, channelsDB) {
-        //Channel.find({stationId : req.query.stationId, 'airings.endTime' : {$gt : startTime, $lt : endTime}}, {_id : false, 'airings.program.preferredImage.uri' : true, 'airings.endTime' : true, 'airings.startTime' : true, 'airings.program.tmsId' : true, 'airings.program.title' : true, callSign : true }, function(err, channelsDB) {
-        //Channel.find({stationId : req.query.stationId}, {airings: {$elemMatch:{endTime: {$gt: startTime, $lt: endTime}}} }, function(err, channelsDB) {
-        //Channel.find({stationId : req.query.stationId}).populate({path: 'airings', match: {endTime:{$gt: startTime, $lt: endTime}}, select:'startTime endTime title program.tmsId, program.preferredImage.uri'}).exec( function(err, channelsDB) {
         Channel.aggregate([{$match: {stationId : req.query.stationId}}, 
                            {$unwind: "$airings"}, 
                            {$match: {"airings.endTime": {$gt: startTime, $lte: endTime}}}, 
