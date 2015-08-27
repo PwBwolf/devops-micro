0198261d09968a09b81034c55ad2245903e7b4de
Achinth Gurkhi <achinth@gmail.com>
2015-08-27 12:54:25 +0530

    new promos provided by marketing

--- server/common/models/cms-ad.js
+++ server/common/models/cms-ad.js
@@ -4,7 +4,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CmsAd = new Schema({
-    name: String
+    name: String,
+    imageFront: String,
+    imageBack: String,
+    description: String
 }, {collection: 'CmsAds'});
 
 mongoose.model('CmsAd', CmsAd);
