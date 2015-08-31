d87f5437d47f568378c32f98839ca9a20de095a0
Achinth Gurkhi <achinth@gmail.com>
2015-08-31 21:39:00 +0530

    change config to app.yiptv.com and ssl

--- server/common/config/development.js
+++ server/common/config/development.js
@@ -21,7 +21,7 @@ module.exports = {
     cloudSpongeDomainKey: 'LJZY53MYH2K7YQKX3TZD',
     complimentarySignUpUrl: 'http://localhost:3000/sign-up/complimentary/',
     refundPeriodInDays: 3,
-    wordPressUrl: 'http://test.yiptv.net/live-tv/',
+    wordPressUrl: 'https://int.yiptv.net/live-tv/',
     metaDataRetrievalRecurrence: '0 0 2 * * *',
     freeSideSelfServiceApiUrl: 'http://172.16.10.5:8080/',
     freeSideFreePremiumUserPackageParts: [17, 18],
