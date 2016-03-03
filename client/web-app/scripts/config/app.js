73734758d19fd9d4e81aa5feaf38fe8fb4a245f4
Achinth Gurkhi <achinth@gmail.com>
2016-03-03 11:59:14 -0500

    google analytics integration

--- client/web-app/scripts/config/app.js
+++ client/web-app/scripts/config/app.js
@@ -3,7 +3,8 @@
 
     angular.module('app', [
         'ngSanitize', 'ngRoute', 'ngAnimate', 'pageslide-directive',
-        'pascalprecht.translate', 'webStorageModule', 'angularPayments', 'ui.bootstrap'
+        'pascalprecht.translate', 'webStorageModule', 'angularPayments', 'ui.bootstrap',
+        'angulartics', 'angulartics.google.analytics'
     ]);
     angular.module('app').value('_', window._);
     angular.module('app').value('toastr', window.toastr);
