a921b0eceaceaaef1b8853aa5385b34ea765df8d
Shawn Carrick <carrickshawn@gmail.com>
2016-03-04 15:35:34 -0500

    need to change branch so im saving this

--- client/web-app/scripts/controllers/epg-controller.js
+++ client/web-app/scripts/controllers/epg-controller.js
@@ -1,5 +1,9 @@
 (function (app){
 	'use strict'
+	console.log('hello')
+	app.controller('epgCtrl', ['$scope', '$rootScope', '$', '$q', '$timeout', 'mediaSvc', '$filter', '$compile', function ($scope, $rootScope, $, $q, $timeout, mediaSvc, $filter, $compile) {
+		console.log('epgCtrl loaded')
+		$scope.blah = 'this is a string for testing'
+	}])
 
-
-}(angular.module('app')))
+}(angular.module('app')));
