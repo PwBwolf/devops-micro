98f1dc725e279027db41a8ce4b0772dcfda1cb69
Achinth Gurkhi <achinth@gmail.com>
2016-04-04 17:32:44 +0530

    fix sign up page for ubs and perkspt to remove 2nd month free and add 20% discount

---
+++ tools/admin-cli/export-cj-users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+
+var config = require('../../server/common/setup/config'),
+    logger = require('../../server/common/setup/logger'),
+    validation = require('../../server/common/services/validation'),
+    mongoose = require('../../server/node_modules/mongoose');
+
+var modelsPath = config.root + '/server/common/models',
+    db = mongoose.connect(config.db);
+
+require('../../server/common/setup/models')(modelsPath);
+var Account = mongoose.model('Account');
+
+Account.find({'merchant': 'CJ'}).populate('primaryUser').exec(function (err, accounts) {
+    if (err) {
+        logger.logError('adminCLI - exportCjUsers - error fetching accounts');
+        logger.logError(err);
+        process.exit(1);
+    } else if (!accounts || accounts.length === 0) {
+        logger.logError('adminCLI - exportCjUsers - no accounts found!');
+        process.exit(0);
+    } else {
+        for (var i = 0; i < accounts.length; i++) {
+            if (accounts[i].primaryUser) {
+                console.log(accounts[i].primaryUser.email + ',' + accounts[i].primaryUser.status + ',' + accounts[i].type);
+            }
+        }
+        process.exit(0);
+    }
+});
