1c43e1a81b125a963443c7622a86166f592f2d3c
Shawn Carrick <carrickshawn@gmail.com>
2016-04-07 16:42:47 -0400

    took out console logs and added semicolon

--- client/web-app/scripts/controllers/player-controller.js
+++ client/web-app/scripts/controllers/player-controller.js
@@ -100,18 +100,8 @@
         $scope.loadMore = function() {
             for(var i = 0; i < 10; i++) {
                 var checkLength = ($scope.programming.length + 1) < $scope.allChannels.length;
-                console.log('\n starting loop logic')
-                console.log('adding new element', checkLength);
-                console.log("i = ", i)
-                console.log("programming length", $scope.programming.length);
-                console.log("programming length + 1", $scope.programming.length + 1);
-                console.log("all channels length", $scope.allChannels.length);
-                console.log('programming array', $scope.programming)
                 if(checkLength) {
-                    //var channel = $scope.allChannels.slice($scope.programming.length + i, $scope.programming.length + i + 1);
-                    console.log($scope.programming.length+1)
-                    var channel = $scope.allChannels[$scope.programming.length+1]
-                    console.log('adding channel', channel)
+                    var channel = $scope.allChannels[$scope.programming.length+1];
                     $scope.programming.push(channel);
                 }
             }
