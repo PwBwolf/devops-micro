3a552f6ef74e2ee0c97d82fa0047ee75848719d0
Achinth Gurkhi <achinth@gmail.com>
2016-04-14 00:27:39 +0530

    tooling

--- tools/admin-cli/paid-user-db-freeside-check.js
+++ tools/admin-cli/paid-user-db-freeside-check.js
@@ -6,6 +6,7 @@ var config = require('../../server/common/setup/config'),
     logger = require('../../server/common/setup/logger'),
     validation = require('../../server/common/services/validation'),
     billing = require('../../server/common/services/billing'),
+    async = require('async'),
     mongoose = require('../../server/node_modules/mongoose');
 
 var modelsPath = config.root + '/server/common/models',
@@ -16,28 +17,45 @@ var Account = mongoose.model('Account');
 
 Account.find({'type': 'paid'}).populate('primaryUser').exec(function (err, accounts) {
     if (err) {
-        logger.logError('adminCLI - paidUserDbFreeSideCheck - error fetching accounts');
+        logger.logError('adminCLI - paidUserDbFreesideCheck - error fetching accounts');
         logger.logError(err);
         process.exit(1);
     } else if (!accounts || accounts.length === 0) {
-        logger.logError('adminCLI - paidUserDbFreeSideCheck - no accounts found!');
+        logger.logError('adminCLI - paidUserDbFreesideCheck - no accounts found!');
         process.exit(0);
     } else {
-        for (var i = 0; i < accounts.length; i++) {
-            var account = accounts[i];
-            var user = accounts[i].primaryUser;
-            var fsPackages = getFreeSidePackages(user, account);
-            if (fsPackages.count !== user.packages.count || user.cancelOn) {
-                console.log(user.email + ',' + user.createdAt.toUTCString() + ',' + user.status + ',' + account.type + ',' + account.packages + ',' +
-                    account.freeSideCustomerNumber + ',' + account.merchant + ',' + account.premiumEndDate.toUTCString() + ',' +
-                    user.upgradeDate.toUTCString() + ',' + user.cancelDate.toUTCString() + ',' + user.cancelOn.toUTCString());
+        var csv = '';
+        async.eachSeries(
+            accounts,
+            function (account, callback) {
+                billing.login(account.primaryUser.email, account.key, account.primaryUser.createdAt.getTime(), function (err, sessionId) {
+                    if (err) {
+                        logger.logError('error logging into freeside for user ' + account.primaryUser.email);
+                        logger.logError(err);
+                        callback();
+                    } else {
+                        billing.getPackages(sessionId, function (err, packages) {
+                            if (err) {
+                                logger.logError(err);
+                                logger.logError('error getting packages for user ' + account.primaryUser.email);
+                                callback();
+                            } else {
+                                var pkg = [];
+                                for (var i = 0; i < packages.length; i++) {
+                                    pkg.push(packages[i].pkg);
+                                }
+                                csv += account.primaryUser.email + ',' + account.packages.length + ',' + pkg.length + ',' + account.primaryUser.cancelOn + '\n';
+                                callback();
+                            }
+                        });
+                    }
+                });
+            },
+            function () {
+                console.log(csv);
+                logger.logInfo('done');
+                process.exit(0);
             }
-        }
-        process.exit(0);
+        );
     }
 });
-
-function getFreeSidePackages(user, account) {
-    billing.login(user.email, )
-}
-
