0fd4a028f8d67d9c2863b1fd2537d5a2144bdd62
Achinth Gurkhi <achinth@gmail.com>
2016-04-22 23:00:12 +0530

    build script changes

--- server/restart-daemons.sh
+++ server/restart-daemons.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 /etc/init.d/yip-rule-engine stop
-/etc/init.d/yip-merchant-processor stop
 /etc/init.d/yip-metadata-processor stop
 /etc/init.d/yip-notification-processor stop
 /etc/init.d/yip-cj-report-processor stop
@@ -9,7 +8,6 @@
 npm update
 
 /etc/init.d/yip-rule-engine start
-/etc/init.d/yip-merchant-processor start
 /etc/init.d/yip-metadata-processor start
 /etc/init.d/yip-notification-processor start
 /etc/init.d/yip-cj-report-processor start
---
+++ server/restart-servers.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+/etc/init.d/yip-api-server stop
+/etc/init.d/yip-web-app stop
+
+npm update
+
+cwd=`pwd`
+cd ../common/database
+node fixtures.js
+cd $cwd
+
+/etc/init.d/yip-api-server start
+/etc/init.d/yip-web-app start
+
+cd ..
+node ../tools/deployment-scripts/notify-build.js
