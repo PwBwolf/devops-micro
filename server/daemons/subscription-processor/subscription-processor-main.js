c91342055e720ad31bf2cc2a49e202271ec7a27e
Achinth Gurkhi <achinth@gmail.com>
2016-05-03 18:49:35 +0530

    subscription processor

--- server/daemons/subscription-processor/subscription-processor-main.js
+++ server/daemons/subscription-processor/subscription-processor-main.js
@@ -5,36 +5,46 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var CronJob = require('cron').CronJob,
     config = require('../../common/setup/config'),
     logger = require('../../common/setup/logger'),
-    subscription = require('../../common/services/subscription'),
     MongoClient = require('mongodb').MongoClient,
     async = require('async'),
-    moment = require('moment');
+    mongoose = require('mongoose'),
+    moment = require('moment'),
+    modelsPath = config.root + '/server/common/models';
 
-MongoClient.connect(config.db, function (err, db) {
+mongoose.connect(config.db, function (err) {
     if (err) {
+        logger.logError('subscriptionProcessorMain - mongoose connection error');
         logger.logError(err);
-        logger.logError('subscriptionProcessorMain - db connection error');
     } else {
-        new CronJob(config.subscriptionProcessorRecurrence, function () {
-            logger.logInfo('subscriptionProcessorMain - subscription processor starting');
-            processTasks(db, function () {
-                logger.logInfo('subscriptionProcessorMain - subscription processor ending');
-            });
-        }, function () {
-            logger.logInfo('subscriptionProcessorMain - subscription processor has stopped');
-        }, true, 'America/Anchorage');
+        require('../../common/setup/models')(modelsPath);
+        var subscription = require('../../common/services/subscription');
+        MongoClient.connect(config.db, function (err, db) {
+            if (err) {
+                logger.logError('subscriptionProcessorMain - db connection error');
+                logger.logError(err);
+            } else {
+                new CronJob(config.subscriptionProcessorRecurrence, function () {
+                    logger.logInfo('subscriptionProcessorMain - subscription processor starting');
+                    processTasks(db, subscription, function () {
+                        logger.logInfo('subscriptionProcessorMain - subscription processor ending');
+                    });
+                }, function () {
+                    logger.logInfo('subscriptionProcessorMain - subscription processor has stopped');
+                }, true, 'America/Anchorage');
+            }
+        });
     }
 });
 
-function processTasks(db, cb) {
+function processTasks(db, subscription, cb) {
     async.waterfall([
         // remove 7 day packages
         function remove7DayPackage(callback) {
             var Accounts = db.collection('Accounts');
             var Users = db.collection('Users');
             var date = moment();
-            date = moment.utc(date.subtract(8, 'days')).startOf('day');
-            var query = {type: 'free', startDate: {$lte: new Date(date.format())}};
+            date = moment.utc(date.subtract(7, 'days')).startOf('day');
+            var query = {type: 'free', packages: {$size: 2}, startDate: {$lte: new Date(date.format())}};
             Accounts.count(query, function (err, accountCount) {
                 if (err) {
                     logger.logError('subscriptionProcessorMain - remove7DayPackage - error fetching account count');
@@ -80,44 +90,34 @@ function processTasks(db, cb) {
         },
         // end complimentary subscription
         function endComplimentarySubscription(callback) {
-            var Accounts = db.collection('Accounts');
             var Users = db.collection('Users');
             var date = moment();
-            date = moment.utc(date.subtract(1, 'days')).startOf('day');
-            var query = {type: 'comp', validTill: {$lte: new Date(date.format())}};
-            Accounts.count(query, function (err, accountCount) {
+            date = moment.utc(date).startOf('day');
+            var query = {validTill: {$lte: new Date(date.format())}};
+            Users.count(query, function (err, userCount) {
                 if (err) {
-                    logger.logError('subscriptionProcessorMain - endComplimentarySubscription - error fetching account count');
+                    logger.logError('subscriptionProcessorMain - endComplimentarySubscription - error fetching user count');
                     logger.logError(err);
                     callback(err);
                 } else {
-                    logger.logInfo('subscriptionProcessorMain - endComplimentarySubscription - account count ' + accountCount);
-                    Accounts.find(query).toArray(function (err, accounts) {
+                    logger.logInfo('subscriptionProcessorMain - endComplimentarySubscription - user count ' + userCount);
+                    Users.find(query).toArray(function (err, users) {
                         if (err) {
-                            logger.logError('subscriptionProcessorMain - endComplimentarySubscription - error fetching accounts');
+                            logger.logError('subscriptionProcessorMain - endComplimentarySubscription - error fetching users');
                             logger.logError(err);
                             callback(err);
                         } else {
                             async.eachSeries(
-                                accounts,
-                                function (account, callback) {
-                                    Users.findOne({account: account._id}, function (err, user) {
-                                        if (err) {
-                                            logger.logError('subscriptionProcessorMain - endComplimentarySubscription - error fetching user');
-                                            logger.logError(err);
+                                users,
+                                function (user, callback) {
+                                    if (user.status === 'active' || user.status === 'registered') {
+                                        subscription.endComplimentarySubscription(user.email, function (err) {
+                                            if (err) {
+                                                logger.logError('subscriptionProcessorMain - endComplimentarySubscription - error ending complimentary subscription ' + user.email);
+                                            }
                                             callback();
-                                        } else if (!user) {
-                                            logger.logError('subscriptionProcessorMain - endComplimentarySubscription - user not found ' + account._id);
-                                            callback();
-                                        } else if (user.status === 'active' || user.status === 'registered') {
-                                            subscription.endComplimentarySubscription(user.email, function (err) {
-                                                if (err) {
-                                                    logger.logError('subscriptionProcessorMain - endComplimentarySubscription - error ending complimentary subscription ' + user.email);
-                                                }
-                                                callback();
-                                            });
-                                        }
-                                    });
+                                        });
+                                    }
                                 },
                                 function () {
                                     callback();
@@ -130,10 +130,9 @@ function processTasks(db, cb) {
         },
         // end premium subscription
         function endPaidSubscription(callback) {
-            var Accounts = db.collection('Accounts');
             var Users = db.collection('Users');
             var date = moment();
-            date = moment.utc(date.subtract(1, 'days')).startOf('day');
+            date = moment.utc(date).startOf('day');
             var query = {cancelOn: {$lte: new Date(date.format())}};
             Users.count(query, function (err, userCount) {
                 if (err) {
